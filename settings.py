f38ac4c71861f39acfa52fdcada58d8ac06b4d58
Werberth <werberthvinicius@gmail.com>
2019-06-02 00:59:54 -0300

    Feat: add author module, Author model and setup database

--- settings.py
+++ settings.py
@@ -1,2 +1,11 @@
+import os
+
 SECRET_KEY = 'you-will-never-guess'
 DEBUG = True
+DB_USERNAME = 'root'
+DB_PASSWORD = '123456'
+BLOG_DATABASE_NAME = 'blog'
+DB_HOST = os.getenv('IP', 'localhost:5050')
+DB_URI = 'mysql+pymysql://%s:%s@%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
