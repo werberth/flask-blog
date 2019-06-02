3f8235283457b6c955ce90e79d9c4a5c2509bfb2
Werberth <werberthvinicius@gmail.com>
2019-06-02 02:41:25 -0300

    Feat: move register and succes views to author.views file

--- author/views.py
+++ author/views.py
@@ -1,5 +1,19 @@
 from flask_blog import app
+from flask import render_template, redirect, url_for
+from author.forms import RegisterForm
 
 @app.route('/login')
 def login():
     return "Hello, User!"
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('author/register.html', form=form)
+
+
+@app.route('/success')
+def success():
+    return "Author registered!"
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from flask_blog import app
-from flask import render_template, redirect, url_for
-from author.forms import RegisterForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World!"
-
-
-@app.route('/register', methods=('GET', 'POST'))
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        return redirect(url_for('success'))
-    return render_template('author/register.html', form=form)
-
-
-@app.route('/success')
-def success():
-    return "Author registered!"
